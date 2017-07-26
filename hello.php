1f0db63157a675386b2904afc3f01546bcb956f5
formation <toto@toto.com>
2017-07-26 15:29:16 +0200

    php mvc Nouveau controller hello

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+// Chargement de la bibliothèque
+require "lib/mvc.php";
+$pageTitle = "Accueil";
+$name = filter_input(INPUT_GET, "name", FILTER_SANITIZE_STRING);
+
+// Récupération de la vue
+$html = getRenderedView("home", [
+    "pageTitle" => $pageTitle,
+    "name" => $name
+]);
+
+echo $html;
